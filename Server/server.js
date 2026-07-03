4887f2e04808ae4a41c6360261aa09847ab55eda
shoaib702 <shoaibsheikh7020@gmail.com>
2026-07-03 23:06:48 +0530

    Add TiDB SSL support

--- Server/server.js
+++ Server/server.js
@@ -24,11 +24,16 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // MySQL connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
 
+  
+  ssl: {
+    rejectUnauthorized: false,  
+  },
+});
 db.connect((err) => {
   if (err) {
     console.error("❌ MySQL Connection Failed:", err.message);
